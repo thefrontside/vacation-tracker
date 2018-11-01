692f47e0d97791fc00b3c9eee7bce692e9c8bdba
David Keathley <keathleydavidj@gmail.com>
2018-11-01 18:00:20 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return ( 
+    <div>
+      <h1>Hello Party People</h1>
+    </div>
+  )
+};
+render( <App/> , document.getElementById("app"));
