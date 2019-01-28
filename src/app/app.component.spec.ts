4cdce905a89977968f6b926d3aec78293883821b
Taras Mankovski <tarasm@gmail.com>
2019-01-28 13:43:39 -0500

    Minor clean up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,11 @@
-import { TestBed, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-
-import { Location, LocationStrategy } from "@angular/common";
-import { MockLocationStrategy, SpyLocation } from "@angular/common/testing";
+import { Location } from "@angular/common";
+import { SpyLocation } from "@angular/common/testing";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-// imports RouterModule.forRoot(routes)
-// imports appRoutingModule
-import { AppModule } from "./app.module";
-
-import { DOCUMENT } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 import { routes } from "../app/app-routing.module";
 
 function replaceLocation(module) {
@@ -44,7 +36,6 @@ class AppTestModule {}
 describe("AppComponent", () => {
   let appModule;
   let appRoot;
-  let location: Location;
 
   beforeEach(async () => {
     appRoot = document.createElement("app-root");
@@ -52,8 +43,7 @@ describe("AppComponent", () => {
     appModule = await platformBrowserDynamic().bootstrapModule(
       AppTestModule
     );
-    location = appModule.injector.get(Location);
-    debugger;
+    let location = appModule.injector.get(Location);
   });
 
   afterEach(async () => {
@@ -65,34 +55,4 @@ describe("AppComponent", () => {
   it("should create the app", () => {
     expect(true).toBeTruthy();
   });
-
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     imports: [
-  //       RouterTestingModule
-  //     ],
-  //     declarations: [
-  //       AppComponent
-  //     ],
-  //   }).compileComponents();
-  // }));
-
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-
-  // it(`should have as title 'vacation-tracker'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('vacation-tracker');
-  // });
-
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to vacation-tracker!');
-  // });
 });
