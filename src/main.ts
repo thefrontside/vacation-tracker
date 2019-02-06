46c006d3660adfa7cf6049aceade0f958aca19b2
David Keathley <david@frontside.io>
2019-02-06 15:08:41 -0600

    Get mirage working on live app

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import startMirage from '../bigtest/network/start';
 
 if (environment.production) {
   enableProdMode();
+}
+
+if (environment.MIRAGE_SCENARIO) {
+  console.log('mirage scenario: ', environment.MIRAGE_SCENARIO);
+  startMirage(environment.MIRAGE_SCENARIO.split(','));
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
